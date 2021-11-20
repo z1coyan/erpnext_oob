fddd2aa8c0a923fb0c01807349cfe70ae82ece83
余则霖 <7624863+yuzelin@user.noreply.gitee.com>
2021-11-20 19:35:09 +0800

    clean up

--- erpnext_oob/erpnext_out_of_box/public/js/hooks/page/dashboard.js
+++
@@ -1,9 +0,0 @@
-
-frappe.pages['dashboard-view'].on_page_show = function(wrapper) {
-	const dashboard = frappe.dashboard;
-	let title = dashboard.dashboard_name;
-	if (dashboard && dashboard.page.title === __('{0} Dashboard', [title])){
-		title = __('{0} Dashboard', [__(title)]);
-		dashboard.page.set_title(title);
-	};
-};
