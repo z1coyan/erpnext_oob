46fe74fd4016dcdd41e6dd6cee6f386940572878
余则霖 <7624863+yuzelin@user.noreply.gitee.com>
2024-10-11 10:35:10 +0800

    excel复制粘贴超50行可使用后台处理

--- erpnext_oob/public/js/form/controls/table copy.js
+++ erpnext_oob/public/js/form/controls/table copy.js
@@ -20,6 +20,7 @@ frappe.ui.form.ControlTable = class ControlTable extends frappe.ui.form.ControlT
 			if (data.length === 1 && data[0].length === 1) return;
 
 			let fieldnames = [];
+			let child_rows = []
 			// for raw data with column header
 			if (this.get_field(data[0][0])) {
 				data[0].forEach(column => {
@@ -41,21 +42,22 @@ frappe.ui.form.ControlTable = class ControlTable extends frappe.ui.form.ControlT
 
 			let row_idx = locals[doctype][row_docname].idx;
 			let data_length = data.length;
-
-			if (data.length <=50 || !table_field_event_handler){
+			let tasks = [];	//setTimeout是异步函数，须确保明细行处理完了再处理整个明细表
+			tasks.push(() => {
 				data.forEach((row, i) => {
+					let child_row = {};
 					setTimeout(() => {
 						let blank_row = !row.filter(Boolean).length;
 						if (!blank_row) {
 							if (row_idx > this.frm.doc[table_field].length) {
 								this.grid.add_new_row();
 							}
-
+	
 							if (row_idx > 1 && (row_idx - 1) % grid_pagination.page_length === 0) {
 								grid_pagination.go_to_page(grid_pagination.page_index + 1);
 							}
-
-							const row_name = grid_rows[row_idx - 1].doc.name;
+	
+							const row_name = grid_rows[row_idx - 1].doc.name;						
 							row.forEach((value, data_index) => {
 								const fieldname =fieldnames[data_index];
 								if (fieldname) {
@@ -71,61 +73,35 @@ frappe.ui.form.ControlTable = class ControlTable extends frappe.ui.form.ControlT
 												value = parseInt(value);
 											}
 										}									
-									};
-									frappe.model.set_value(doctype, row_name, fieldname, value);
+									 };
+									if (!table_field_event_handler){ 
+										frappe.model.set_value(doctype, row_name, fieldname, value);
+									}else{	//如果有子表事件，则不触发明细行字段事件，由子表事件调用后台统一处理									
+										child_row[fieldname] = value;
+									}
 								}
 							});
+							child_rows.push(child_row);
+							console.log(child_row);
 							row_idx++;
 						}
 						//fisher 移到外面来，复制的最后空行不会导致进度条走不完
 						if (data_length >= 10) {
 							let progress = i + 1;
 							frappe.show_progress(__('Processing'), progress, data_length, null, true);
-						}
-					}, 0);
+						}	
+					}, 0);				
 				});
-				return false; // Prevent the default handler from running.
-			} else {
-				let child_rows = []
-				data.forEach((row, i) => {
-					let child_row = {};				
-					let blank_row = !row.filter(Boolean).length;
-					if (!blank_row) {
-						if (row_idx > this.frm.doc[table_field].length) {
-							this.grid.add_new_row();
-						}
+			})
+			
+			tasks.push(() => {
+				if (table_field_event_handler){
+					table_field_event_handler(this.frm, child_rows);
+				}			
+			})
 
-						if (row_idx > 1 && (row_idx - 1) % grid_pagination.page_length === 0) {
-							grid_pagination.go_to_page(grid_pagination.page_index + 1);
-						}
-
-						const row_name = grid_rows[row_idx - 1].doc.name;
-						row.forEach((value, data_index) => {
-							const fieldname =fieldnames[data_index];
-							if (fieldname) {
-								//fisher 保存文本编辑器字段的回车换行
-								if (value) {
-									const fieldtype = frappe.meta.get_field(doctype,fieldname).fieldtype;
-									if (fieldtype){
-										if (fieldtype ==='Text Editor') {
-											value = value.replace(/[\n\r]/g,'<br>');
-										} else if (['Float', 'Currency'].includes(fieldtype)) {
-											value = parseFloat(value);
-										} else if (fieldtype ==='Int') {
-											value = parseInt(value);
-										}
-									}									
-								};
-								child_row[fieldname] = value;
-							}
-						});
-						child_rows.push(child_row);
-						row_idx++;
-					}					
-				});
-				table_field_event_handler(this.frm, child_rows);
-				return false; // Prevent the default handler from running.
-			}
+			frappe.run_serially(tasks);
+			return false; // Prevent the default handler from running.
 		});
 	}
 };
